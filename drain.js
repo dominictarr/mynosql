59ce234e711c522ff3477439fd212eec1b6ee5a3
Dominic Tarr <dominic.tarr@gmail.com>
2015-02-18 13:15:27 +1300

    remove old files

--- drain.js
+++
@@ -1,8 +0,0 @@
-
-
-//notify the user when the database does not currently have
-//and in flight writes.
-
-module.exports = function (db) {
-
-}
--- example/db.js
+++
@@ -1,5 +0,0 @@
-var level = require('level')
-var sublevel = require('level-sublevel/bytewise')
-
-module.exports = sublevel(level('/tmp/mynosql-npm-example', {encoding: 'json'}))
-
